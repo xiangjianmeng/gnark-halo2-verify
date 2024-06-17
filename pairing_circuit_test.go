076973da4d387386b190821e29be3d3d714b9ec1
xiangjianmeng <805442788@qq.com>
2024-06-17 18:37:01 +0800

    bn256 pairing passed

--- pairing_circuit_test.go
+++ pairing_circuit_test.go
@@ -16,11 +16,9 @@ import (
 )
 
 func setupKeys() (groth16.ProvingKey, groth16.VerifyingKey, constraint.ConstraintSystem) {
-	var defaultG1 = &sw_bn254.G1Affine{}
-	var defaultG2 = &sw_bn254.G2Affine{}
-	var circuit = BN256PairingCircuit{
-		G1Points: [2]*sw_bn254.G1Affine{defaultG1, defaultG1},
-		G2Points: [2]*sw_bn254.G2Affine{defaultG2, defaultG2},
+	circuit := BN256PairingCircuit{
+		G1Points: [2]*sw_bn254.G1Affine{new(sw_bn254.G1Affine), new(sw_bn254.G1Affine)},
+		G2Points: [2]*sw_bn254.G2Affine{new(sw_bn254.G2Affine), new(sw_bn254.G2Affine)},
 	}
 
 	r1cs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
@@ -44,18 +42,30 @@ func createProof(pk groth16.ProvingKey, r1cs constraint.ConstraintSystem) (groth
 
 	// Set the G1 and G2 points
 	var g10 = bn254.G1Affine{}
-	_, err := g10.X.SetString("9711642498692685419534775182903675958746183064939871544167697980170875863155")
+	_, err := g10.X.SetString("2920616387084030925907755037226454382846345550621956833249622258647667607078")
 	if err != nil {
 		panic(err)
 	}
-	_, err = g10.Y.SetString("2060437998766753388036284772870047840593529666943632769057824771468919582800")
+	_, err = g10.Y.SetString("16502678157049327323910877548707266122319935523346850792311342099791838736912")
 	if err != nil {
 		panic(err)
 	}
-	log.Println("g10.IsOnCurve", g10.IsOnCurve())
+	log.Println("g10GAff.IsOnCurve", g10.IsOnCurve())
+
+	var g11 = bn254.G1Affine{}
+	_, err = g11.X.SetString("7162082828732168937516361335135022403650719329242056518668518043784043198510")
+	if err != nil {
+		panic(err)
+	}
+	_, err = g11.Y.SetString("18768486256042060147567165227366772940689098806912976766549486364846889365307")
+	if err != nil {
+		panic(err)
+	}
+	log.Println("g11GAff.IsOnCurve", g11.IsOnCurve())
 
 	swG10 := sw_bn254.NewG1Affine(g10)
-	witnessCircuit.G1Points = [2]*sw_bn254.G1Affine{&swG10, &swG10}
+	swG11 := sw_bn254.NewG1Affine(g11)
+	witnessCircuit.G1Points = [2]*sw_bn254.G1Affine{&swG10, &swG11}
 
 	var g20 = bn254.G2Jac{}
 	g20.X.SetString(
@@ -116,6 +126,86 @@ func verifyProof(proof groth16.Proof, vk groth16.VerifyingKey, witness witness.W
 	return true
 }
 
+func TestPairingCheckTestSolve(t *testing.T) {
+	witnessCircuit := BN256PairingCircuit{}
+	circuit := BN256PairingCircuit{
+		G1Points: [2]*sw_bn254.G1Affine{new(sw_bn254.G1Affine), new(sw_bn254.G1Affine)},
+		G2Points: [2]*sw_bn254.G2Affine{new(sw_bn254.G2Affine), new(sw_bn254.G2Affine)},
+	}
+
+	assert := test.NewAssert(t)
+	var g10 = bn254.G1Affine{}
+	_, err := g10.X.SetString("2920616387084030925907755037226454382846345550621956833249622258647667607078")
+	if err != nil {
+		panic(err)
+	}
+	_, err = g10.Y.SetString("16502678157049327323910877548707266122319935523346850792311342099791838736912")
+	if err != nil {
+		panic(err)
+	}
+	//g10.Z.SetOne()
+	//var g10GAff bn254.G1Affine
+	//g10GAff.FromJacobian(&g10)
+	log.Println("g10GAff.IsOnCurve", g10.IsOnCurve())
+
+	var g11 = bn254.G1Affine{}
+	_, err = g11.X.SetString("7162082828732168937516361335135022403650719329242056518668518043784043198510")
+	if err != nil {
+		panic(err)
+	}
+	_, err = g11.Y.SetString("18768486256042060147567165227366772940689098806912976766549486364846889365307")
+	if err != nil {
+		panic(err)
+	}
+	//g11.Z.SetOne()
+	//var g11GAff bn254.G1Affine
+	//g11GAff.FromJacobian(&g11)
+	log.Println("g11GAff.IsOnCurve", g11.IsOnCurve())
+
+	log.Println("GenAff IsOnCurve", g10.IsOnCurve(), g11.IsOnCurve())
+	swG10 := sw_bn254.NewG1Affine(g10)
+	//g11GAff.Neg(&g10GAff)
+	swG11 := sw_bn254.NewG1Affine(g11)
+	witnessCircuit.G1Points = [2]*sw_bn254.G1Affine{&swG10, &swG11}
+
+	var g20 = bn254.G2Jac{}
+	g20.X.SetString(
+		"13560776910958896778309428450808722712362583089620853136731412215646945297830",
+		"17006984783035916014836003241042945838018391379603479329912940809520477208052",
+	)
+	g20.Y.SetString(
+		"18091431719069275340788954644764755415339503689597559484185985670418402491565",
+		"12900589361605111856143562381318004229723309245412571888359653960504992734777",
+	)
+	g20.Z.SetString("1", "0")
+	log.Println("g20.IsOnCurve", g20.IsOnCurve())
+
+	var g21 = bn254.G2Jac{}
+	g21.X.SetString(
+		"10857046999023057135944570762232829481370756359578518086990519993285655852781",
+		"11559732032986387107991004021392285783925812861821192530917403151452391805634",
+	)
+	g21.Y.SetString(
+		"13392588948715843804641432497768002650278120570034223513918757245338268106653",
+		"17805874995975841540914202342111839520379459829704422454583296818431106115052",
+	)
+	g21.Z.SetString("1", "0")
+	log.Println("g21.IsOnCurve", g21.IsOnCurve())
+
+	var g20GenAff, g21GenAff bn254.G2Affine
+	g20GenAff.FromJacobian(&g20)
+	g21GenAff.FromJacobian(&g21)
+	log.Println("GenAff IsOnCurve", g20GenAff.IsOnCurve(), g21GenAff.IsOnCurve())
+
+	swG20 := sw_bn254.NewG2Affine(g20GenAff)
+	swG21 := sw_bn254.NewG2Affine(g21GenAff)
+
+	witnessCircuit.G2Points = [2]*sw_bn254.G2Affine{&swG20, &swG21}
+
+	err = test.IsSolved(&circuit, &witnessCircuit, ecc.BN254.ScalarField())
+	assert.NoError(err)
+}
+
 func TestPairing(t *testing.T) {
 	log.Println("setupKeys start !")
 	pk, vk, r1cs := setupKeys()
@@ -137,17 +227,35 @@ func TestOnCurve(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	// proof[102]
-	var g10 = bn254.G1Affine{}
-	_, err := g10.X.SetString("9711642498692685419534775182903675958746183064939871544167697980170875863155")
+	var g10 = bn254.G1Jac{}
+	_, err := g10.X.SetString("2920616387084030925907755037226454382846345550621956833249622258647667607078")
 	if err != nil {
-		return
+		panic(err)
 	}
-	// proof[103]
-	_, err = g10.Y.SetString("2060437998766753388036284772870047840593529666943632769057824771468919582800")
+	_, err = g10.Y.SetString("16502678157049327323910877548707266122319935523346850792311342099791838736912")
 	if err != nil {
-		return
+		panic(err)
 	}
+	g10.Z.SetOne()
 	assert.True(g10.IsOnCurve())
+	var g10GAff bn254.G1Affine
+	g10GAff.FromJacobian(&g10)
+	assert.True(g10GAff.IsOnCurve())
+
+	var g11 = bn254.G1Jac{}
+	_, err = g11.X.SetString("7162082828732168937516361335135022403650719329242056518668518043784043198510")
+	if err != nil {
+		panic(err)
+	}
+	_, err = g11.Y.SetString("18768486256042060147567165227366772940689098806912976766549486364846889365307")
+	if err != nil {
+		panic(err)
+	}
+	g11.Z.SetOne()
+	assert.True(g11.IsOnCurve())
+	var g11GAff bn254.G1Affine
+	g11GAff.FromJacobian(&g11)
+	assert.True(g11GAff.IsOnCurve())
 
 	var g20 = bn254.G2Jac{}
 	g20.X.SetString(
